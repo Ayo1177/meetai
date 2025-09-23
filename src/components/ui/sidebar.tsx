40116dcf81f88deff38279d3ad0aa05c784d245f
Ayoub Karroum <ayoubkarroum5@gmail.com>
2025-09-24 00:16:55 +0100

    deploy53

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, VariantProps } from "class-variance-authority"
-import { PanelLeftIcon } from "lucide-react"
+import { PanelLeftIcon, PanelLeftCloseIcon } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -258,7 +258,7 @@ function SidebarTrigger({
   onClick,
   ...props
 }: React.ComponentProps<typeof Button>) {
-  const { toggleSidebar } = useSidebar()
+  const { toggleSidebar, state, isMobile } = useSidebar()
 
   return (
     <Button
@@ -273,7 +273,10 @@ function SidebarTrigger({
       }}
       {...props}
     >
-      <PanelLeftIcon />
+      {(state === "collapsed" || isMobile)
+        ? <PanelLeftIcon className="size-4" />
+        : <PanelLeftCloseIcon className="size-4" />
+      }
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
